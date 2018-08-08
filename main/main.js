ae0967a5a617f7f02f169ef3bb0a8fb68e797086
tf.fak <theshark556@gmail.com>
2018-08-08 02:05:02 -0400

    Change Window Name.
    Change Window Name to IG:DM+

--- main/main.js
+++ main/main.js
@@ -11,7 +11,7 @@ const autoUpdater = require('./autoupdater');
 // fixes electron's timeout inconsistency
 // not doing this on windows because the fix doesn't work for windows.
 if (process.platform != 'win32') {
-  require('./timeout-shim').fix();
+    require('./timeout-shim').fix();
 }
 
 const RATE_LIMIT_DELAY = 10000;
@@ -22,118 +22,123 @@ let pollingInterval = 1000;
 let mainWindow
 let session
 
-function createWindow () {
-  if (!mainWindow) {
-    mainWindow = new BrowserWindow({
-      width: 1200,
-      height: 800,
-      icon: `${__dirname}/../browser/img/icon.png`,
-      minWidth: 500,
-      minHeight: 400
+function createWindow() {
+    if (!mainWindow) {
+        mainWindow = new BrowserWindow({
+            width: 1200,
+            height: 800,
+            icon: `${__dirname}/../browser/img/icon.png`,
+            minWidth: 500,
+            minHeight: 400
+        })
+    }
+    mainWindow.setTitle('IG:DM+')
+
+    instagram.checkAuth(session).then((result) => {
+        let view = result.isLoggedIn ? '../browser/index.html' : '../browser/login.html'
+        session = result.session || session
+
+        mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname, view),
+            protocol: 'file:',
+            slashes: true
+        }))
     })
-  }
-  mainWindow.setTitle('Insta:DM')
 
-  instagram.checkAuth(session).then((result) => {
-    let view = result.isLoggedIn ? '../browser/index.html' : '../browser/login.html'
-    session = result.session || session
-
-    mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, view),
-      protocol: 'file:',
-      slashes: true
-    }))
-  })
-
-  mainWindow.on('closed', () => mainWindow = null)
+    mainWindow.on('closed', () => mainWindow = null)
 }
 
-function getChatList () {
-  instagram.getChatList(session).then((chats) => {
-    mainWindow.webContents.send('chatList', chats)
+function getChatList() {
+    instagram.getChatList(session).then((chats) => {
+        mainWindow.webContents.send('chatList', chats)
 
-    setTimeout(getChatList, pollingInterval);
-  }).catch(() => setTimeout(getChatList, RATE_LIMIT_DELAY))
+        setTimeout(getChatList, pollingInterval);
+    }).catch(() => setTimeout(getChatList, RATE_LIMIT_DELAY))
+}
+
+function deleteMessage(messageID) {
+
 }
 
 let timeoutObj;
 let messagesThread;
-function getChat (evt, id) {
-  // used to get older messages, see #getOlderMessages
-  if (messagesThread && messagesThread.threadId != id) {
-    messagesThread = null
-  }
 
-  instagram.getChat(session, id).then((chat) => {
-    mainWindow.webContents.send('chat', chat);
+function getChat(evt, id) {
+    // used to get older messages, see #getOlderMessages
+    if (messagesThread && messagesThread.threadId != id) {
+        messagesThread = null
+    }
 
-    if (timeoutObj) clearTimeout(timeoutObj)
+    instagram.getChat(session, id).then((chat) => {
+        mainWindow.webContents.send('chat', chat);
 
-    timeoutObj = setTimeout(getChat, pollingInterval, {}, id);
-  }).catch(() => setTimeout(getChat, RATE_LIMIT_DELAY, evt, id))
+        if (timeoutObj) clearTimeout(timeoutObj)
+
+        timeoutObj = setTimeout(getChat, pollingInterval, {}, id);
+    }).catch(() => setTimeout(getChat, RATE_LIMIT_DELAY, evt, id))
 }
 
 app.on('ready', () => {
-  createWindow();
-  // only set the menu template when in production mode/
-  // this also leaves the dev console enabled when in dev mode.
-  if (!process.defaultApp) {
-    const menu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(menu); 
-  }
-  autoUpdater.init();
+    createWindow();
+    // only set the menu template when in production mode/
+    // this also leaves the dev console enabled when in dev mode.
+    if (!process.defaultApp) {
+        const menu = Menu.buildFromTemplate(menuTemplate);
+        Menu.setApplicationMenu(menu);
+    }
+    autoUpdater.init();
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  // only call createWindow afeter mainWindow is set to null at
-  // mainWindow.on('closed')
-  if (mainWindow === null) createWindow()
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    // only call createWindow afeter mainWindow is set to null at
+    // mainWindow.on('closed')
+    if (mainWindow === null) createWindow()
 })
 
 // reduce polling frequency when app is not active.
 app.on('browser-window-blur', () => {
-  pollingInterval = 30000;
+    pollingInterval = 30000;
 })
 
 app.on('browser-window-focus', () => {
-  pollingInterval = 10000;
-  app.setBadgeCount(0);
+    pollingInterval = 10000;
+    app.setBadgeCount(0);
 })
 
 electron.ipcMain.on('login', (evt, data) => {
-  if(data.username === "" || data.password === "") {
-    return mainWindow.webContents.send('loginError', "Please enter all required fields");
-  }
-  instagram.login(data.username, data.password).then((session_) => {
-    session = session_
-    createWindow()
-  }).catch((error) => {
-    let message = 'An unknown error occurred.';
-    if (error.message) {
-      message = error.message;
-    } else if (error.hasOwnProperty('json') && !!error.json.two_factor_required) {
-      message = 'You have two factor authentication enabled. Two factor authentication is not yet supported.';
+    if (data.username === "" || data.password === "") {
+        return mainWindow.webContents.send('loginError', "Please enter all required fields");
     }
-    mainWindow.webContents.send('loginError', message);
-  })
+    instagram.login(data.username, data.password).then((session_) => {
+        session = session_
+        createWindow()
+    }).catch((error) => {
+        let message = 'An unknown error occurred.';
+        if (error.message) {
+            message = error.message;
+        } else if (error.hasOwnProperty('json') && !!error.json.two_factor_required) {
+            message = 'You have two factor authentication enabled. Two factor authentication is not yet supported.';
+        }
+        mainWindow.webContents.send('loginError', message);
+    })
 })
 
 electron.ipcMain.on('logout', () => {
-  instagram.logout()
-  session = null
-  createWindow()
+    instagram.logout()
+    session = null
+    createWindow()
 })
 
 electron.ipcMain.on('getLoggedInUser', () => {
-  instagram.getLoggedInUser(session).then((user) => {
-    mainWindow.webContents.send('loggedInUser', user);
-  })
+    instagram.getLoggedInUser(session).then((user) => {
+        mainWindow.webContents.send('loggedInUser', user);
+    })
 })
 
 electron.ipcMain.on('getChatList', getChatList)
@@ -141,48 +146,48 @@ electron.ipcMain.on('getChatList', getChatList)
 electron.ipcMain.on('getChat', getChat)
 
 electron.ipcMain.on('getOlderMessages', (_, id) => {
-  instagram.getOlderMessages(session, messagesThread, id)
-    .then((data) => {
-      messagesThread = data.thread
-      mainWindow.webContents.send('olderMessages', data.messages)
-    })
+    instagram.getOlderMessages(session, messagesThread, id)
+        .then((data) => {
+            messagesThread = data.thread
+            mainWindow.webContents.send('olderMessages', data.messages)
+        })
 })
 
 electron.ipcMain.on('message', (_, data) => {
-  if (data.isNewChat) {
-    instagram.sendNewChatMessage(session, data.message, data.users).then((chat) => getChat(null, chat[0].id))
-  } else {
-    instagram.sendMessage(session, data.message, data.chatId)
-  }
+    if (data.isNewChat) {
+        instagram.sendNewChatMessage(session, data.message, data.users).then((chat) => getChat(null, chat[0].id))
+    } else {
+        instagram.sendMessage(session, data.message, data.chatId)
+    }
 })
 
 electron.ipcMain.on('upload', (_, data) => {
-  instagram.uploadFile(session, data.filePath, data.recipients)
-    .then((chat) => {
-      if (data.isNewChat) getChat(null, chat[0].id)
-    })
+    instagram.uploadFile(session, data.filePath, data.recipients)
+        .then((chat) => {
+            if (data.isNewChat) getChat(null, chat[0].id)
+        })
 })
 
 electron.ipcMain.on('searchUsers', (_, search) => {
-  instagram.searchUsers(session, search).then((users) => {
-    mainWindow.webContents.send('searchResult', users);
-  })
+    instagram.searchUsers(session, search).then((users) => {
+        mainWindow.webContents.send('searchResult', users);
+    })
 })
 
 electron.ipcMain.on('markAsRead', (_, thread) => {
-  instagram.seen(session, thread)
+    instagram.seen(session, thread)
 })
 
 electron.ipcMain.on('increase-badge-count', (_) => {
-  app.setBadgeCount(app.getBadgeCount() + 1);
+    app.setBadgeCount(app.getBadgeCount() + 1);
 })
 
 electron.ipcMain.on('getUnfollowers', (_) => {
-  instagram.getUnfollowers(session).then((users) => {
-    mainWindow.webContents.send('unfollowers', users)
-  })
+    instagram.getUnfollowers(session).then((users) => {
+        mainWindow.webContents.send('unfollowers', users)
+    })
 })
 
 electron.ipcMain.on('unfollow', (_, userId) => {
-  instagram.unfollow(session, userId)
+    instagram.unfollow(session, userId)
 })
